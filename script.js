10c7c4afe2be10e3c339bdc4b447454743f915a9
Lucas <lucas.rocha2021@outlook.com>
2024-06-16 21:48:42 -0300

    Corrigindo problemas do projeto
    Pach notes :
    
    1 : Corrigido o problema ao qual o usuario não seleciona o tipo do operador e o sistema retornava "Undefined".
    
    2 : Corrigido o problema ao qual ao selecionar o operador de divisão mas não informando nenhum valor o sistema retornava NaN.
    
    3 : Melhoria na interface, mas ainda será feito um novo commit trazendo novas melhorias no projeto.

--- script.js
+++ script.js
@@ -1,101 +1,42 @@
-let num1;
-let num2;
-let operador;
-let resultado;
+var num1;
+var num2;
+var operador;
+var resultado;
 
 function apagarcampos() {
     num1 = document.querySelector('#num1').value = '';
     num2 = document.querySelector('#num2').value = '';
+    document.querySelector('#resultado').innerHTML = '';
 }
 function calcular() {
     num1 = document.querySelector('#num1');
     num2 = document.querySelector('#num2');
     operador = document.querySelector('#options');
 
-    operador = operacao.value; //Obtendo o valor do <option> selecionado
+    let operacao = operador.value; // Obtendo o valor do <option> selecionado
 
     var n1 = Number(num1.value);
     var n2 = Number(num2.value);
 
-    if (operador == 'soma') {
-        resultado = n1 + n2;
-    }
-    if (operador == 'subtracao') {
-        resultado = n1 - n2;
-    }
-    if (operador == 'multiplicacao') {
-        resultado = n1 * n2;
-    }
-    if (operador == 'divisao') {
-        resultado = n1 / n2;
+    if (isNaN(n1) || isNaN(n2)) {
+        resultado = "Erro: valor inválido";
+    } else {
+        if (operacao === 'soma') {
+            resultado = n1 + n2;
+        } else if (operacao === 'subtracao') {
+            resultado = n1 - n2;
+        } else if (operacao === 'multiplicacao') {
+            resultado = n1 * n2;
+        } else if (operacao === 'divisao') {
+            if (n2 === 0) {
+                resultado = "Erro: divisão por zero";
+            } else {
+                resultado = n1 / n2;
+            }
+        } else {
+            resultado = "Erro: operador inválido";
+        }
     }
     document.querySelector('#resultado').innerHTML = resultado;
 }
-
-
-
-
-
-
 console.log(operador);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*function calcular() {
-    num1 = document.querySelector(".num1").value;
-    num2 = document.querySelector(".num2").value;
-    var resultado = parseInt(num1) + parseInt(num2);
-    document.querySelector(".resultado").innerHTML = resultado;
-}
-function options() {
-    let select = document.querySelector("#options").value;
-    switch (options) {
-        case soma:
-            num1 = document.querySelector(".num1").value;
-            num2 = document.querySelector(".num2").value;
-            var resultado = parseInt(num1) + parseInt(num2);
-            document.querySelector(".resultado").innerHTML = resultado;
-            break;
-
-        case subtracao:
-            num1 = document.querySelector(".num1").value;
-            num2 = document.querySelector(".num2").value;
-            var resultado = parseInt(num1) - parseInt(num2);
-            document.querySelector(".resultado").innerHTML = resultado;
-            break;
-
-        case divisao:
-            num1 = document.querySelector(".num1").value;
-            num2 = document.querySelector(".num2").value;
-            var resultado = parseInt(num1) / parseInt(num2);
-            document.querySelector(".resultado").innerHTML = resultado;
-            break;
-        case multiplicacao:
-            num1 = document.querySelector(".num1").value;
-            num2 = document.querySelector(".num2").value;
-            var resultado = parseInt(num1) * parseInt(num2);
-            document.querySelector(".resultado").innerHTML = resultado;
-            break;
-    }
-
-}*/
-
-
-
